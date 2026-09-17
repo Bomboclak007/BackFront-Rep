5993f36aab7fb611948a4a2f6c4ae9b34ef31e7b
Bomboclack <158288280+Bomboclak007@users.noreply.github.com>
2026-09-17 16:41:38 +0200

    Update file.js

--- front/file.js
+++ front/file.js
@@ -7,7 +7,7 @@ import imageThree from '../front/public/Image3.png'
 import imageFor from '../front/public/Image4.png'
 import like from '../front/public/like.png'
 import rep from '../front/public/rep.png'
-
+import arrow from '../front/public/arrow.png'
 
 export let Photo = {
 
@@ -17,4 +17,5 @@ export let Photo = {
     imageThree:imageThree,
     imageFor:imageFor,
     rep:rep,
+    arrow:arrow,
 }
